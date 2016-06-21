833a6df513c6d695eea4a1de25a3ff5a288f7b4e
Fuyuan Wang <zoeblow@163.com>
2016-06-21 17:28:53 +0800

    V1.2

--- dist/script/showToast.js
+++ dist/script/showToast.js
@@ -1,7 +1,7 @@
 /*
- * @filename showToast.js
- * @author fy[2015-05-19 13:31]
- * @update fy[2015-06-01 13:56]
+ * @filename showToast.css
+ * @author fy[2016-05-19 13:31]
+ * @update fy[2016-06-21 17:26]
  * @version v1.1
  * @description 显示toast
  * @基于zepto/jquery
@@ -9,7 +9,8 @@
 
 /*
 * V 1.1 更新 可以配置 时间 如果为空则默认为1500毫秒
-* 
+* 2016年6月21日17:26
+* V 1.2 针对IOS系统调整Toast出现位置 
 */
 
 var showToast=(function(){
