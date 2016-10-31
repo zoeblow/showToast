ab93717ff9a4d80ff0612a48546ee4ee30cd70f9
Fuyuan Wang <zoeblow@gmail.com>
2016-10-31 15:13:35 +0800

    1.3 add showimg toast

--- README.md
+++ README.md
@@ -20,23 +20,31 @@ showToast 是一款移动端的Toast控件，可以在移动端展示Toast效果
 > * 修改时间 ：2016-06-21 17:26
 > * 描述：针对IOS系统调整Toast出现位置 
 
+------
+
+> * V 1.3
+> * 修改时间 ：2016年10月31日15:02
+> * 描述：增加显示toast 有小图标的功能 修改若干现实bug,修改了传参方式
+
 -----
 ### Js代码
 ```JavaScript
 var showToast=(function(){
     var instance=null;
-        function show(text,timer){
-            if(!timer){
-                timer = 1500;
+        function show(obj){
+            var that = this;
+            that.obj = {
+                timer : that.text,
+                timer : that.timer || 1500,
             }
             if(!instance){
-                var $container=$('<div class="showToast"><div class="showToastInner"><div class="showToastTips fieldTipBounceIn"><div class="showToastCon">'+text+'</div></div></div></div>');
+                var $container=$('<div class="showToast"><div class="showToastInner"><div class="showToastTips fieldTipBounceIn"><div class="showToastCon">'+obj.text+'</div></div></div></div>');
                 $container.appendTo($("body"));
                 instance=true;
                 setTimeout(function(){
                     $container.remove();
                     instance=false;
-                },timer)
+                },obj.timer)
             }
         }
    
@@ -44,6 +52,7 @@ var showToast=(function(){
         show:show
     }
 })();
+
 ```
 -----
 ###调用方式
@@ -53,13 +62,17 @@ var showToast=(function(){
 <script type="text/javascript" src="script/zepto.min.js"></script>
 <script type="text/javascript" src="script/showToast.js"></script>
 <script>
-    //调用
-    showToast.show('请输入企业名称',1000);
+    //调用没有小图标版本
+    showToast.show({text:'这就是页面显示的文字喽',timer:2000})
+
+    //调用没有小图标版本
+    showToastImg.show({text:'这就是页面显示的文字喽',timer:2000,icon:'success'})
 </script>
 ```
 -----
 ###参数
 | 参数    | 取值类型   |  默认值  | 描述|
 | :----:  | :----:  | :----:  | :----:  |
-| text    | String |   空     | 页面上显示的文字  |
-| timer    | Number |   1500 |   滞留在页面上的时间|
+| text    | String |   必输    | 页面上显示的文字  |
+| timer    | Number |   1500 |   滞留在页面上的时间 |
+| icon    | String |   null | 显示图标的样式,可为空 可选值为"success,fail,error,busy" |
--- dist/script/showToast.js
+++ dist/script/showToast.js
@@ -11,22 +11,53 @@
 * V 1.1 更新 可以配置 时间 如果为空则默认为1500毫秒
 * 2016年6月21日17:26
 * V 1.2 针对IOS系统调整Toast出现位置 
+* ------
+* 2016年10月31日15:02
+* V1.3 增加显示toast 有小图标的功能 修改若干现实bug
 */
 
 var showToast=(function(){
     var instance=null;
-        function show(text,timer){
-            if(!timer){
-                timer = 1500;
+        function show(obj){
+            var that = this;
+            that.obj = {
+                timer : that.text,
+                timer : that.timer || 1500,
             }
             if(!instance){
-                var $container=$('<div class="showToast"><div class="showToastInner"><div class="showToastTips fieldTipBounceIn"><div class="showToastCon">'+text+'</div></div></div></div>');
+                var $container=$('<div class="showToast"><div class="showToastInner"><div class="showToastTips fieldTipBounceIn"><div class="showToastCon">'+obj.text+'</div></div></div></div>');
                 $container.appendTo($("body"));
                 instance=true;
                 setTimeout(function(){
                     $container.remove();
                     instance=false;
-                },timer)
+                },obj.timer)
+            }
+        }
+   
+    return {
+        show:show
+    }
+})();
+
+var showToastImg=(function(){
+    var instance=null;
+        function show(obj){
+            console.log(obj.text)
+            var that = this;
+            that.obj = {
+                timer : that.text,
+                timer : that.timer || 1500,
+                icon : that.icon || "icontest"
+            }
+            if(!instance){
+                var $container=$('<div class="showToastImg"><div class="showMask"></div><div class="showToastInner"><div class="showToastTips fieldTipBounceIn"><div class="showToastCon"><span class="icontest '+obj.icon+'"></span><span class="text">'+obj.text+'</span></div></div></div></div>');
+                $container.appendTo($("body"));
+                instance=true;
+                setTimeout(function(){
+                    $container.remove();
+                    instance=false;
+                },obj.timer)
             }
         }
    
